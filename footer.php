8cfa95e34cb5e41ffdcd9abb0926dc8a138b4f1a
Aaron Graham <aegraham@gmail.com>
2014-07-10 14:45:15 -0500

    Chrome Frame has been retired, more info here: https://developers.google.com/chrome/chrome-frame/ Removed from SoM theme some time ago.

--- footer.php
+++ footer.php
@@ -42,10 +42,6 @@ if (isset($_GET['post_type']) && $_GET['post_type'] == 'event' && is_search()) {
 
 
 
-<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
-		 http://chromium.org/developers/how-tos/chrome-frame-getting-started -->
-<!--[if lt IE 7]><p class="chromeframe">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-
 <?php
    /* Always have wp_footer() just before the closing </body>
 	* tag of your theme, or you will break many plugins, which
