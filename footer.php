84ab38f0841fec70d83cb743912dc0500eea6116
Sam Hermes <samhermes@mpa-l-hermes.wucon.wustl.edu>
2015-04-16 14:36:01 -0500

    SVG icons, fix for background hover state of campaign banner, footer spacing adjustments

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 </div>
 <footer class="clearfix">
-	<div class="campaign-wrapper">
-		<div class="wrapper">
-			<a class="campaign-banner" href="http://together.wustl.edu" onclick="__gaTracker('send','event','outbound-campaign','http://together.wustl.edu');">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/campaign.png" alt="Leading Together, the campaign for Washington University">
-			</a>
+	<a class="campaign-banner" href="http://together.wustl.edu" onclick="__gaTracker('send','event','outbound-campaign','http://together.wustl.edu');">
+		<div class="campaign-wrapper">
+			<div class="wrapper">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/campaign.png" alt="Leading Together, the campaign for Washington University">
+			</div>
 		</div>
-	</div>
-	<div class="wrapper">
+	</a>
+	<div class="wrapper clearfix">
 		<div class="double-footer">
 			<p>Our 2,100 employed and volunteer faculty — the <a href="http://wuphysicians.wustl.edu" onclick="__gaTracker('send','event','outbound-footer','http://wuphysicians.wustl.edu');">Washington University Physicians</a> — also are the medical staff of <a href="http://www.barnesjewish.org/" onclick="__gaTracker('send','event','outbound-footer','http://www.barnesjewish.org');">Barnes-Jewish</a> and <a href="http://www.stlouischildrens.org/" onclick="__gaTracker('send','event','outbound-footer','http://www.stlouischildrens.org');">St. Louis Children's</a> hospitals. Through its hospital affiliations, the School is linked to BJC HealthCare.</p>
 			<p class="small-text"><a href="http://www.wustl.edu/policies/copyright.html">&copy; <?php echo date("Y") ?> Washington University in St. Louis</a></p>
@@ -24,12 +24,12 @@
 		</div>
 		<div class="single-footer last-child">
 			<p class="social">
-				<a title="Facebook" class="facebook" onclick="__gaTracker('send','event','outbound-footer-social','facebook');" href="https://www.facebook.com/WUSTLmedicine.health"></a>
-				<a title="Twitter" class="twitter" onclick="__gaTracker('send','event','outbound-footer-social','twitter');" href="http://twitter.com/WUSTLmed"></a>
-				<a title="LinkedIn" class="linkedin" onclick="__gaTracker('send','event','outbound-footer-social','linkedin');" href="https://www.linkedin.com/company/washington-university-school-of-medicine"></a>
-				<a title="Google Plus" class="googleplus" onclick="__gaTracker('send','event','outbound-footer-social','googleplus');" href="http://plus.google.com/+WashingtonUniversitySchoolofMedicineStLouis/"></a>
-				<a title="RSS feed" class="rss" onclick="__gaTracker('send','event','outbound-footer-social','rss');" href="<?php bloginfo('rss2_url'); ?>"></a>
-				<a title="WUSTL Reader mobile app" class="reader" onclick="__gaTracker('send','event','outbound-footer-social','WUSTL reader');" href="http://reader.wustl.edu/"></a>
+				<a title="Facebook" class="facebook" onclick="__gaTracker('send','event','outbound-footer-social','facebook');" href="https://www.facebook.com/WUSTLmedicine.health"><img src="<?php echo get_template_directory_uri(); ?>/_/img/facebook.svg" width="23" height="23" onerror="this.src='<?php echo get_template_directory_uri(); ?>/_/img/facebook.png';this.onerror=null;"></a>
+				<a title="Twitter" class="twitter" onclick="__gaTracker('send','event','outbound-footer-social','twitter');" href="http://twitter.com/WUSTLmed"><img src="<?php echo get_template_directory_uri(); ?>/_/img/twitter.svg" width="23" height="23" onerror="this.src='<?php echo get_template_directory_uri(); ?>/_/img/twitter.png';this.onerror=null;"></a>
+				<a title="LinkedIn" class="linkedin" onclick="__gaTracker('send','event','outbound-footer-social','linkedin');" href="https://www.linkedin.com/company/washington-university-school-of-medicine"><img src="<?php echo get_template_directory_uri(); ?>/_/img/linkedin.svg" width="23" height="23" onerror="this.src='<?php echo get_template_directory_uri(); ?>/_/img/linkedin.png';this.onerror=null;"></a>
+				<a title="Google Plus" class="googleplus" onclick="__gaTracker('send','event','outbound-footer-social','googleplus');" href="http://plus.google.com/+WashingtonUniversitySchoolofMedicineStLouis/"><img src="<?php echo get_template_directory_uri(); ?>/_/img/google-plus.svg" width="23" height="23" onerror="this.src='<?php echo get_template_directory_uri(); ?>/_/img/google-plus.png';this.onerror=null;"></a>
+				<a title="RSS feed" class="rss" onclick="__gaTracker('send','event','outbound-footer-social','rss');" href="<?php bloginfo('rss2_url'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/_/img/rss.svg" width="23" height="23" onerror="this.src='<?php echo get_template_directory_uri(); ?>/_/img/rss.png';this.onerror=null;"></a>
+				<a title="WUSTL Reader mobile app" class="reader" onclick="__gaTracker('send','event','outbound-footer-social','WUSTL reader');" href="http://reader.wustl.edu/"><img src="<?php echo get_template_directory_uri(); ?>/_/img/reader.png" width="23" height="23"></a>
 			</p>
 			<p class="small-text pad-top-20">660 S. Euclid Ave.<br>St. Louis, MO 63110</p>
 		</div>
--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form name="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label class="visuallyhidden" for="search-box">Search</label>
     <input type="text" id="search-box" name="s" value="<?php echo ( is_search() ) ? get_search_query() : "Search" ;?>" onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search';}" placeholder="Search">
-    <div class="submit" name="submit" id="search-btn" onclick="document.getElementById('search-form').submit();"><img alt="Search" src="<?php echo get_template_directory_uri(); ?>/_/img/search.png"></div>
+    <div class="submit" name="submit" id="search-btn" onclick="document.getElementById('search-form').submit();"><img alt="Search" src="<?php echo get_template_directory_uri(); ?>/_/img/search.svg" onerror="this.src='<?php echo get_template_directory_uri(); ?>/_/img/search.png';this.onerror=null;"></div>
 </form>
