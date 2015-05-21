97a1a07f27045663285dcb267b9ae6b88b344c51
Brian Marston <bmarston@wustl.edu>
2015-05-21 16:33:05 -0500

    Make the photos on Campus Life cards clickable

--- _/php/campus-life-template.php
+++ _/php/campus-life-template.php
@@ -1,15 +1,15 @@
 <div class="card">
-    <?php the_post_thumbnail( array(325, 218) ); ?>
+    <?php
+        $external_link = get_field('external_link');
+        $url = $external_link['url'] ? $external_link['url'] : get_permalink();
+        $link_text = $external_link['title'] ? $external_link['title'] : 'See photos';
+    ?>
+
+    <a href="<?php echo $url; ?>"><?php the_post_thumbnail( array(325, 218) ); ?></a>
 
     <ul>
         <li>
             <div class="dateline"><span class="date"><?php the_time('M j, Y'); ?></span></div>
-            <?php
-            $external_link = get_field('external_link');
-
-            $url = $external_link['url'] ? $external_link['url'] : get_permalink();
-            $link_text = $external_link['title'] ? $external_link['title'] : 'See photos';
-            ?>
             <a href="<?php echo $url; ?>"><?php the_title(); ?></a>
             <p><?php echo get_the_excerpt(); ?> <a class="more" href="<?php echo $url; ?>"><?php echo $link_text; ?></a></p>
         </li>
