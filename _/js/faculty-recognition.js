aa0db512f375c9ca542bf08a1d2a64cece3778f1
Sam Hermes <sam.hermes@wustl.edu>
2015-07-07 15:52:16 -0500

    Remove unused JS

--- _/js/faculty-recognition.js
+++ _/js/faculty-recognition.js
@@ -22,6 +22,7 @@ jQuery(document).ready(function() {
 			success:function(data) {
 				// This outputs the result of the ajax request
 				jQuery('#award-years-right').html(data);
+				// Call matchHeight again for new cards
 				jQuery('.faculty-individual').matchHeight();
 			},
 			error: function(errorThrown){
@@ -29,68 +30,4 @@ jQuery(document).ready(function() {
 			}
 		});
 	});
-
-	jQuery(".photo-container a:nth-child(4)").addClass("fourth");
-		
-	jQuery(".spacer-container").hover(
-		function() {
-			jQuery(this).children(".view-all").slideToggle();
-		}
-	);
-		
-	jQuery(".left-arrow").click(function() {
-		p = jQuery(this).prevAll(".photo-container");
-		c = -1 * p.pixels('left');
-		t = p.children().length * 81;
-		if( c > 648 ) {
-			p.children(".fourth").removeClass("fourth");
-			m = 648;
-			p.animate({left: '+=648'});
-			jQuery(p.children()[(c - m) / 81 + 3]).addClass('fourth');
-		} else if( c > 0 ) {
-			p.children(".fourth").removeClass("fourth");
-			m = 0;
-			p.animate({left: m});
-			jQuery(p.children()[3]).addClass('fourth');
-		} else {
-			p.effect("bounce", { times: 3, direction: 'right', distance: 5 }, 100);
-		}
-		
-	});
-		
-	jQuery(".right-arrow").click(function() {
-		p = jQuery(this).prevAll(".photo-container");
-		c = -1 * p.pixels('left');
-		t = p.children().length * 81;
-		
-		if( (c + 648 + 648 ) < t ) {
-			p.children(".fourth").removeClass("fourth");
-			m = 648;
-			p.animate({left: '-=648'});
-			jQuery(p.children()[(c + m) / 81 + 3]).addClass('fourth');
-		} else if( c + 648 < t ) {
-			p.children(".fourth").removeClass("fourth");
-			m = t - (c + 648 );
-			p.animate({left: '-=' + m});
-			jQuery(p.children()[(c + m) / 81 + 3]).addClass('fourth');
-		} else {
-			p.effect("bounce", { times: 3, direction: 'left', distance: 5 }, 100);
-		}
-	});
-		
-	jQuery('.faculty-photo').mousemove(function(e){
-		var x = e.pageX + 1;
-		var y = e.pageY - 23;
-		jQuery("#hover").css('left', x);
-		jQuery("#hover").css('top', y);
-	}).hover(function() {
-		jQuery("#hover").html(jQuery(this).attr('title'));
-	});
-		
-	jQuery('.photo-container').mouseenter(function() {
-		jQuery("#hover").show();
-	}).mouseleave(function() {
-		jQuery("#hover").hide();
-	});
-	
 });
