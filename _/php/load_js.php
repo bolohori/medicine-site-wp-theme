f2485420109a9f3b16d60368b280e4f03f992a24
Sam Hermes <sam.hermes@wustl.edu>
2015-07-30 16:13:21 -0500

    Only enqueue FitVids on pages with video embeds, minify jquery.fitvids.js

--- _/php/load_js.php
+++ _/php/load_js.php
@@ -8,7 +8,7 @@ function queue_wusm_scripts() {
 		wp_enqueue_script('front_page_js', get_stylesheet_directory_uri() . '/_/js/front_page.js', array( 'bxslider' ) );
 	}
 
-	wp_enqueue_script('fitvids', get_stylesheet_directory_uri() . '/_/js/jquery.fitvids.js', array( 'jquery' ) );
+	wp_register_script('wusm-fitvids', get_template_directory_uri() . '/_/js/jquery.fitvids.min.js', array( 'jquery' ), '1.1', true );
 
 	wp_enqueue_script('custom_functions', get_stylesheet_directory_uri() . '/_/js/functions.js', array( 'jquery' ) );
 
@@ -19,3 +19,16 @@ function queue_wusm_scripts() {
     }
 }
 add_action('wp_enqueue_scripts', 'queue_wusm_scripts');
+
+function wusm_embed_container( $html, $url, $attr ) {
+	// Bail if this is the admin
+	if ( is_admin() ) {
+		return $html;
+	}
+	if ( isset( $attr['width'] ) ) {
+		wp_enqueue_script( 'wusm-fitvids' );
+	}
+	return $html;
+}
+add_filter( 'embed_handler_html', 'wusm_embed_container', 10, 3 );
+add_filter( 'embed_oembed_html' , 'wusm_embed_container', 10, 3 );
