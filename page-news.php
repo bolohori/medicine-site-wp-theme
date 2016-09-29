efa452dbac935e9d2be6ebf26121d9aca68bea6d
Zachary Flauaus <zflauaus@wustl.edu>
2016-09-29 13:47:23 -0500

    Added back play button on editors pick.

--- page-news.php
+++ page-news.php
@@ -25,11 +25,18 @@ if (have_posts()) :
             if ( $the_query->have_posts() ) { ?>
             <div class="editors-pick">
                 <?php while ( $the_query->have_posts() ) {
-                    $the_query->the_post(); 
+                    $the_query->the_post();
                     $exclude[] = $post->ID; ?>
                     <a href="<?php ( get_field('url') ? the_field('url') : the_permalink() ) ?>">
                     <div class="ep-card">
-                        <?php the_post_thumbnail('news'); ?>
+                        <?php if(get_field('featured_video_url')) { ?>
+                            <div class="video-card-image">
+                                <?php the_post_thumbnail('news'); ?>
+                                <img src="<?php echo get_template_directory_uri() . '/_/img/play.png' ?>" class="video-play-icon">
+                            </div>
+                        <?php } else {
+                            the_post_thumbnail('news');
+                        } ?>
                         <div class="editors-pick-text">
                             <p class="article-date"><?php the_time( get_option( 'date_format' ) ); ?></p>
                             <?php the_title('<h2>', '</h2>'); ?>
@@ -82,7 +89,7 @@ if (have_posts()) :
                 <ul class="clearfix">
                 <?php while ( $the_query->have_posts() ) {
                     $the_query->the_post();
-                    
+
                     get_template_part( '_/php/news/card' );
 
                     } ?>
