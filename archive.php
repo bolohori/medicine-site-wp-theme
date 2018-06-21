f4e939cacfbccb503202f96d638f07405df65624
Aaron Graham <aaron.graham@wustl.edu>
2018-06-21 14:26:38 -0500

    Fix /for-media/news-releases and move new template up one level in template heirarchy

--- archive.php
+++ archive.php
@@ -1,71 +1,27 @@
 <?php
-if ( is_category() || is_tax() ) {
-	$type_type = 'news';
-	$post_type = get_query_var( 'news' );
-} else {
-	$type_type = 'post_type';
-	$post_type = get_post_type();
-}
 
-$num_to_fetch = apply_filters( "{$post_type}_num_per_page", 30 );
-
-get_header(); ?>
-<div id="page-background"></div>
-	<div id="main" class="clearfix">
-	<div class="wrapper clearfix">
-		<div id="page-background-inner"></div>
-		<?php get_sidebar(); ?>
-
+get_header();
+	?>
+	<div id="main" class="page-news clearfix">
+		<?php get_template_part( '_/php/news/header' ); ?>
 		<article>
-			<h1><?php the_archive_title(); ?></h1>
+			<div class="news-type-title">
+				<p><?php the_archive_title(); ?></p>
+			</div>
 			<?php
-				$template = locate_template( "_/php/headers/$post_type.php" );
-				if ( '' !== $template ) {
-					get_template_part( "_/php/headers/$post_type" );
-				}
+			if ( have_posts() ) {
 			?>
-			<?php if ( have_posts() ) { ?>
+			<div class="news-cards">
+				<ul class="clearfix">
 				<?php
-				if ( 0 === $paged ) {
-					// WP_Query arguments
-					$args = array(
-						$type_type       => $post_type,
-						'posts_per_page' => $num_to_fetch,
-						'orderby'        => 'menu_order',
-						'order'          => 'ASC',
-						'meta_key'       => 'sticky',
-						'meta_value'     => 1,
-					);
-
-					// The Query
-					$query = new WP_Query( $args );
-
-					if ( $query->have_posts() ) {
-						while ( $query->have_posts() ) {
-
-							$query->the_post();
-							$template = locate_template( "_/php/cards/$post_type.php" );
-							if ( '' === $template ) {
-								get_template_part( '_/php/news/card' );
-							} else {
-								get_template_part( "_/php/cards/$post_type" );
-							}
-						}
-						echo '<hr>';
-						wp_reset_postdata();
-					}
-				}
 				while ( have_posts() ) {
 					the_post();
 
-					$template = locate_template( "_/php/cards/$post_type.php" );
-					if ( '' === $template ) {
-						get_template_part( '_/php/news/card' );
-					} else {
-						get_template_part( "_/php/cards/$post_type" );
-					}
+					get_template_part( '_/php/news/card' );
+
 				}
 ?>
+				</ul>
 				<nav class="navigation pagination" role="navigation">
 					<h2 class="screen-reader-text">Posts navigation</h2>
 					<div class="nav-links">
@@ -80,11 +36,14 @@ get_header(); ?>
 								'total'   => $wp_query->max_num_pages,
 							)
 						);
-				?>
+					?>
 					</div>
 				</nav>
-			<?php } ?>
+			</div>
+			<?php
+			}
+			?>
 		</article>
-	</div>
+
 	</div>
 <?php get_footer(); ?>
