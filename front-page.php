23ee36a65cc0fab029d416fc28af606643a9aa94
Zachary Flauaus <zflauaus@wustl.edu>
2018-09-04 11:10:33 -0500

    Beginning rest of Homepage work.
    * Added grunt
    * Migrating CSS to a SASS workflow (slowly)
    * Removed unnecessary images from old homepage
    * Removed unnecessary ACF groups

--- front-page.php
+++ front-page.php
@@ -55,7 +55,7 @@ if ( get_field( 'hero_image', 'option' ) ) {
 		echo '<div class="hero-text">';
 			echo '<span class="hero-headline">' . esc_html( $heroheadline ) . '</span>';
 			echo '<p>' . esc_html( $herodesc ) . '</p>';
-			echo '<a data-category="Front page" data-action="CTA - Hero banner - Admissions" class="hero-button" href="' . esc_html( $herobuttonlink ) . '">' . implode( ' ', $explodedbutton ) . ' ' . '<span class="homebuttonwrap">' . esc_html( $herobuttonlast ) . '</span>' . '</a>';
+			echo '<a data-category="Front page" data-action="CTA - Hero banner - Admissions" class="cta-button" href="' . esc_html( $herobuttonlink ) . '">' . implode( ' ', $explodedbutton ) . ' ' . '<span class="cta-button-wrap">' . esc_html( $herobuttonlast ) . '</span>' . '</a>';
 		echo '</div>';
 	echo '</div>';
 }
@@ -68,136 +68,79 @@ if ( get_field( 'hero_image', 'option' ) ) {
 	}
 ?>
 
-<section class="news-home">
-	<div class="text-intro">
-		<div class="wrap">
-			<h2>We connect the brightest minds in medicine.</h2>
-			<p>With passion and determination, our teachers, scholars, caregivers and innovators come together to tackle medicine's toughest challenges.</p>
-		</div>
+<section class="bythenumbers">
+	<h2>&#35;WashUMed</h2>
+	<span class="tagline">by the numbers</span>
+	<div class="icon">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/home/microscope-icon.svg" alt="microscope-icon">
+		<span class="desctext">Top 10 Medical School</span>	
 	</div>
-	<section class="news">
-		<div class="news-cards">
-			<ul class="clearfix">
-				<li class="news-block">
-					<div>
-						<a data-category="Front page" data-action="CTA - See our latest news" href="/news">
-							<div class="card latest-news-block">
-								<p>Explore the scientific discovery, medical innovation and commitment to care that inspire us.</p>
-								<span class="link">See our latest news</span>
-							</div>
-						</a>
-					</div>
-				</li>
-				<?php
-					if ( have_rows( 'home_stories', 'option' ) ) {
-						while ( have_rows( 'home_stories', 'option' ) ) : the_row();
-							$newscards = get_sub_field( 'selected_stories', 'option' );
-							if ( $newscards ) {
-								$post = $newscards;
-								setup_postdata( $post );
-								get_template_part( '_/php/news/card' );
-							}
-						endwhile;
-					}
-				?>
-			</ul>
-		</div>
-	</section>
-</section>
-
-<section class="cta-home turquoise">
-	<div class="wrap">
-		<div class="cta-text">
-			<h2>We're turning ideals into action.</h2>
-			<p>The School of Medicine is taking meaningful action to increase diversity on campus and in medical professions.</p>
-		</div>
-		<a data-category="Front page" data-action="CTA - Diversity" href="/about/diversity-inclusion/" class="cta-button">SEE DIVERSITY INITIATIVES</a>
+	<div class="icon">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/home/graduation-icon.svg" alt="graduation-icon">
+		<span class="desctext">13 Degree Programs</span>	
+	</div>
+	<div class="icon">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/home/hospital-icon.svg" alt="hospital-icon">
+		<span class="desctext">76 Clinical Specialties and Subspecialties</span>	
+	</div>
+	<div class="icon">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/home/debt-icon.svg" alt="debt-icon">
+		<span class="desctext">2nd-Lowest Student Debt</span>	
 	</div>
 </section>
 
-<?php do_action( 'wusm_front_page_additional_sections' );?>
-
-<section class="leaders-home">
-	<div class="text-intro">
-		<div class="wrap">
-			<h2>We are a community of leaders.</h2>
-			<p>Engaged in our fields at home and around the world, we are defining the future of health and medicine.</p>
-		</div>
+<section class="half-callout">
+	<div class="half-intro">
+		<h2><?php the_field( 'half_callout_title', 'option' ); ?></h2>
 	</div>
-	<section class="leaders">
-		<?php
-		$args = array(
-			'post_type'      => 'post',
-			'news'           => 'national-leaders',
-			'posts_per_page' => 3,
-			'orderby'        => 'date'
-		);
-		$the_query = new WP_Query( $args );
-
-		if ( $the_query->have_posts() ) { ?>
-			<div class="news-cards">
-				<ul class="clearfix">
-					<?php while ( $the_query->have_posts() ) {
-						$the_query->the_post();
-						get_template_part( '_/php/news/card' );
-					} ?>
-				</ul>
-			</div>
-		<?php
-		}
-		wp_reset_postdata(); ?>
-		<a class="national-leaders-see-all" href="/news/type/national-leaders/">SEE ALL</a>
-	</section>
-</section>
-
-<section class="cta-home">
-	<div class="wrap">
-		<div class="cta-text">
-			<h2>The future of medicine starts here.</h2>
-			<p>Our top-ranked, student-centered programs will prepare you to join the next generation of leaders.</p>
-		</div>
-		<a data-category="Front page" data-action="CTA - Admissions button" href="/education/admissions/" class="cta-button">Explore our programs</a>
+	<div class="half-info">
+		<h3><?php the_field( 'half_callout_subhead', 'option' ); ?></h3>
+		<p><?php the_field( 'half_callout_description', 'option' ); ?></p>
+		<a class="hero-button" href="<?php the_field( 'half_callout_link', 'option' ); ?>"><?php the_field( 'half_callout_link_text', 'option' ); ?></a>
 	</div>
 </section>
 
-<section class="icons-home">
-	<div class="wrap">
-		<a data-category="Front page" data-action="Icon - Facts and figures" href="https://otm.wustl.edu/facts-figures/" class="iconlink">
-			<div class="icon">
-				<object type="image/svg+xml" data="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/svg/lightbulb.svg">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/lightbulb.png" srcset="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/lightbulb-2x.png 2x" alt="lightbulb icon">
-					<span class="visuallyhidden">lightbulb icon</span>
-				</object>
-				<div class="context">209 patents filed by faculty and students in FY2016</div>
-			</div>
-		</a>
-		<a data-category="Front page" data-action="Icon - Specialties" href="https://wuphysicians.wustl.edu/medical-services/specialties" class="iconlink">
-			<div class="icon">
-				<object type="image/svg+xml" data="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/svg/76.svg">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/76.png" srcset="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/76-2x.png 2x" alt="76">
-					<span class="visuallyhidden">76</span>
-				</object>
-				<div class="context">clinical specialties and subspecialties</div>
-			</div>
-		</a>
-		<a data-category="Front page" data-action="Icon - Nobel Laureates" href="/research/nobel-prize-winners/" class="iconlink">
-			<div class="icon nobel">
-				<object>
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/nobel-transparent.png" srcset="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/nobel-transparent-2x.png 2x" alt="nobel laureates">
-					<span class="visuallyhidden">nobel laureates</span>
-				</object>
-				<div class="context">18 nobel laureates associated with the school</div>
-			</div>
-		</a>
-		<a data-category="Front page" data-action="Icon - Patient care"  href="/patient-care/" class="iconlink">
-			<div class="icon">
-				<object type="image/svg+xml" data="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/svg/stethoscope.svg">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/stethoscope.png" srcset="<?php echo get_stylesheet_directory_uri(); ?>/_/img/icons/png/stethoscope-2x.png 2x" alt="lightbulb icon">
-					<span class="visuallyhidden">stethoscope icon</span>
-				</object>
-				<div class="context">67% of faculty members also treat patients</div>
-			</div>
-		</a>
+<section class="news-section">
+	<h2>Latest News</h2>
+
+	<span class="more-link">Read all news</span>
+</section>
+
+<section class="showcase-section">
+	<h2><?php the_field( 'showcase_headline', 'option' ); ?></h2>
+	<p><?php the_field( 'showcase_caption', 'option' ); ?></p>
+</section>
+
+<section class="voices-section">
+	<h2><?php the_field( 'full_callout_title', 'option' ); ?></h2>
+	<div class="callout-quote">
+		<h3><?php the_field( 'full_callout_quote', 'option' ); ?></h3>
+		<span class="quote-attribution"><?php the_field( 'full_callout_quote_attribution', 'option' ); ?></span>
+		<span class="attributor-description"><?php the_field( 'full_callout_attributor_description', 'option' ); ?></span>
+		<a class="hero-button" href="<?php the_field( 'full_callout_cta_link', 'option' ); ?>">Read more</a>
+	</div>
+</section>
+
+<section class="leaders-section">
+	<h2><?php the_field( 'community_leaders_title', 'option' ); ?></h2>
+	<div class="leader-lineup">
+	
+	</div>
+	<span class="leader-description"><?php the_field( 'community_leaders_description', 'option' ); ?></span>
+</section>
+
+<section class="connect-section">
+	<h2><?php the_field( 'full_callout_2_title', 'option' ); ?></h2>
+	<p class="connect-subhead"><?php the_field( 'full_callout_2_subhead', 'option' ); ?></p>
+	<a href="hero-button" href="<?php the_field( 'full_callout_2_link', 'option' ); ?>"><?php the_field( 'full_callout_2_link_text' ); ?></a>
+</section>
+
+<section class="full-section">
+	<div class="full-split-left">
+		
+	</div>
+	<div class="full-split-right">
+	
 	</div>
 </section>
 
