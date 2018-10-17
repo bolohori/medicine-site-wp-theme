9c41db6c683e5cd59d7514641b1d9e9dd64e52aa
Aaron Graham <aaron.graham@wustl.edu>
2018-10-17 15:22:31 -0500

    👌 IMPROVE: Fix most of the styling that was off in the author header block

--- _/php/news/author.php
+++ _/php/news/author.php
@@ -4,18 +4,21 @@ global $curauth;
 
 <header class="clearfix news-author-header article-header">
 	<a href="/news" class="visit-news-hub"><div class="arrow-left"></div>Visit the News Hub</a>
-	<img src="
-	<?php
-	$image_array = get_field( 'author_headshot', 'user_' . $curauth->ID );
-	echo $image_array['sizes']['author-headshot'];
-	?>" class='alignleft' alt="">
-	<h1><?php echo $curauth->display_name; ?></h1>
-	<h2><?php echo $curauth->title; ?></h2>
-	<ul class='author-contact-list'>
-		<li class='phone'><?php echo $curauth->phone; ?></li>
-		<li class='email'><a href='mailto:<?php echo $curauth->user_email; ?>'><?php echo $curauth->user_email; ?></a></li>
-		<li class='web'><a href='//publicafairs.med.wustl.edu'>Medcial Public Affairs</a></li>
-	</ul>
-	<p><?php echo $curauth->description;?></p>
-	
+	<div class="author-block">
+		<img src="
+		<?php
+		$image_array = get_field( 'author_headshot', 'user_' . $curauth->ID );
+		echo $image_array['sizes']['author-headshot'];
+		?>" class='alignleft author-headshot' alt="">
+		<div class="author-info">
+			<h1><?php echo $curauth->display_name; ?></h1>
+			<h2><?php echo $curauth->title; ?></h2>
+			<ul class='author-contact-list'>
+				<li class='phone'><?php echo $curauth->phone; ?></li>
+				<li class='email'><a href='mailto:<?php echo $curauth->user_email; ?>'><?php echo $curauth->user_email; ?></a></li>
+				<li class='web'><a href='//publicafairs.med.wustl.edu'>Medcial Public Affairs</a></li>
+			</ul>
+			<p><?php echo $curauth->description;?></p>
+		</div>
+	</div>
 </header>
